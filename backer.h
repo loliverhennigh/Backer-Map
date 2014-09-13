cf7b1efd00bdbc4c58e7f56d35a8afb336c94eb4
Oliver Hennigh <loliverhennigh101@gmail.com>
2014-09-13 17:09:45 -0400

    starting off

---
+++ backer.h
@@ -0,0 +1,15 @@
+
+
+typedef struct {
+	point ** system_a;
+	point ** system_b;
+	unsigned int size;
+} backer;
+
+backer * backer_create(unsigned int size_x, unsigned int size_y, unsigned int number_points);
+void run_inter(backer * b);
+void run_pert(backer * b);
+
+
+
+
---
+++ point.h
@@ -0,0 +1,15 @@
+
+
+typedef struct {
+	unsigned int size_x;
+	unsigned int size_y;
+	bool x;
+	bool y;
+
+} point;
+
+point * backer_create(unsigned int size_x, unsigned int size_y);
+	
+
+
+
